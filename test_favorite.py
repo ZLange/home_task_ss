443c312a0b50464b0b702752bb7830c6a4725f63
ZLange <lange.zaiga@gmail.com>
2021-05-07 17:13:31 +0300

    add to favorites just and ad from ad page

--- test_favorite.py
+++ test_favorite.py
@@ -1,4 +1,5 @@
 import pytest
+from time import sleep
 
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.keys import Keys
@@ -11,8 +12,37 @@ def browser():
     driver.quit()
 
 
-def test_open_ad(browser):
+def test_add_favorite(browser):
     url = 'https://www.ss.com/en'
     browser.get(url)
 
-    assert 'SS.COM' in browser.title, 'Wrong page!' #test
+    assert 'SS.COM' in browser.title, 'Wrong page!'
+
+    browser.find_element_by_id('mtd_97').click()  # cars
+    sleep(2)
+    assert 'Cars' in browser.title, 'Wrong section!'
+
+    browser.find_element_by_xpath("//select[@name='opt[35]']/option[text()='Manual']").click()
+    sleep(5)
+
+    browser.find_element_by_xpath("//form[@id='filter_frm']/table[2]/tbody/tr[5]").click()
+    sleep(5)
+
+    browser.find_element_by_xpath("/html//a[@id='a_fav']").click() # add to favorites
+    sleep(5)
+
+    browser.find_element_by_xpath("/html//a[@id='alert_ok']").click()
+    sleep(5)
+
+    browser.find_element_by_xpath(
+        "//div[@id='main_table']/span[@class='page_header_menu']/span/b[@class='menu_main']/a[@title='Memo']").click()
+    sleep(5)
+    memo_row = len(browser.find_elements_by_xpath("//form[@id='filter_frm']/table/tbody/tr"))
+
+    assert int(memo_row) > 1, "No ad's are added!"
+
+
+
+
+
+
