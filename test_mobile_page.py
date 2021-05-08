90e325609960057e275928419473f9d33793d57a
ZLange <lange.zaiga@gmail.com>
2021-05-08 22:11:44 +0300

    started mobile page test

--- test_mobile_page.py
+++ test_mobile_page.py
@@ -1,24 +1,41 @@
-# import pytest
-# from time import sleep
-#
-# from selenium.webdriver import Chrome
-# from selenium.webdriver.common.keys import Keys
-#
-# @pytest.fixture
-# def browser():
-#     driver = Chrome()
-#     driver.implicitly_wait(10)
-#     yield driver
-#     driver.quit()
-#
-#
-# def test_remove_fav(browser):
-#     url = 'https://www.ss.com/en'
-#     browser.get(url)
-#     browser.Manage().Window.Size = (240, 360)
-#
-#     assert 'SS.COM' in browser.title, 'Wrong page!'
-#
-#     browser.find_element_by_id('mtd_97').click()  # cars
-#     sleep(2)
-#     assert 'Cars' in browser.title, 'Wrong section!'
+import pytest
+from time import sleep
+
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.keys import Keys
+
+@pytest.fixture
+def browser():
+    driver = Chrome()
+    driver.implicitly_wait(10)
+    yield driver
+    driver.quit()
+
+
+def test_remove_fav(browser):
+    url = 'https://m.ss.com/en'
+    browser.get(url)
+
+    assert 'SS.COM' in browser.title, 'Wrong page!'
+
+    browser.find_element_by_xpath("//div[@id='main']//a[@href='/en/transport/']/div[@class='cat2']").click()
+    sleep(2)
+    assert 'Transport' in browser.title, 'Wrong section!'
+
+    browser.find_element_by_xpath("//div[@id='main']//a[@href='/en/transport/cars/']").click()
+    sleep(2)
+    assert 'Cars' in browser.title, 'Wrong section!'
+
+    browser.find_element_by_xpath("//div[@id='main']//a[@href='/en/transport/cars/mercedes/']").click()
+    sleep(2)
+    assert 'Mercedes' in browser.title, 'Wrong section!'
+
+    browser.find_element_by_xpath("//div[@id='main']//a[@href='/en/transport/cars/mercedes/clk-class/']").click()
+    sleep(2)
+    assert 'CLK-class' in browser.title, 'Wrong section!'
+
+    browser.find_element_by_xpath("//form[@id='top_form']/table//tr/td[2]//select[@name='sid']").click()
+    sleep(2)
+
+    # browser.find_element_by_xpath("//form[@id='top_form']/table//tr/td[2]//select[@name='sid']").click()
+    # sleep(2)
